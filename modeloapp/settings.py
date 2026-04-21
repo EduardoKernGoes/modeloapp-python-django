a1d89d730fd0ea5a6a640b64916ff91c8a81afe4
Eduardo Kern Goes da Silva <edugoes215@gmail.com>
2026-04-20 23:21:12 -0300

    Ajuste layout html base

--- modeloapp/settings.py
+++ modeloapp/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [ 
