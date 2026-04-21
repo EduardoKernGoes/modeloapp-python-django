00cc4cd2627208dd66e35e937869e689d500e031
Eduardo Kern Goes da Silva <edugoes215@gmail.com>
2026-04-20 22:52:09 -0300

    Finalização back-end e integração com o front-end, matéria de sexta

--- apps/categories/views.py
+++ apps/categories/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from rest_framework import viewsets
 from .models import Category
 from .serializer import CategorySerializer
+from .forms import CategoryForm
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 # Create your views here.
@@ -9,3 +12,46 @@ from .serializer import CategorySerializer
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+def add_category(request):
+    template_name = 'categories/add_category.html'
+    context = {}
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            f = form.save(commit=False)
+            f.save()
+            form.save_m2m()
+            return redirect('categories:list_categories')
+    form = CategoryForm()
+    context['form'] = form
+    return render(request, template_name, context)
+
+def list_categories(request):
+    template_name = 'categories/list_categories.html'
+    categories = Category.objects.filter()
+    context = {
+        'categories': categories
+    }
+    return render(request, template_name, context)
+
+def edit_category(request, id_category):
+    template_name = 'categories/add_category.html'
+    context ={}
+    category = get_object_or_404(Category, id=id_category)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('categories:list_categories')
+    form = CategoryForm(instance=category)
+    context['form'] = form
+    return render(request, template_name, context)
+
+def delete_category(request, id_category):
+    category = Category.objects.get(id=id_category)
+    category.delete()
+    return redirect('categories:list_categories')
--- modeloapp/settings.py
+++ modeloapp/settings.py
@@ -50,6 +50,12 @@ INSTALLED_APPS = [
     'socialNetwork.apps.SocialnetworkConfig',
     'employees.apps.EmployeesConfig',
     'clients.apps.ClientsConfig',
+    'orders.apps.OrdersConfig',
+    'orderitems.apps.OrderitemsConfig',
+    'invoices.apps.InvoicesConfig',
+    'django_filters',
+    'core.apps.CoreConfig',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -67,7 +73,7 @@ ROOT_URLCONF = 'modeloapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -141,3 +147,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [ 
+    os.path.join(BASE_DIR, 'staticfiles'), 
+]
+
+MEDIA_URL = '/media/' 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
